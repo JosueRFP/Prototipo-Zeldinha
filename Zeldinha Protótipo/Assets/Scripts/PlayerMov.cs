a9fe54491cce9e925d621a57d3a62895b45c5577
JosueRFP <josue.petri.senac@gmail.com>
2025-03-13 13:28:05 -0300

    Movimentação Player concluida

---
+++ Zeldinha Protótipo/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMov : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float vertical, horizontal;
+    [SerializeField] float speed;
+    public float Speed { get => speed; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal, vertical) * Speed;
+    }
+}

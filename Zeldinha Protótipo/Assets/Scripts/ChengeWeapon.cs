017b1c98b8e5b4ddb31e1355fa7134cfd8900b0f
JosueRafP <josue.petri.senac@gmail.com>
2025-03-25 10:48:23 -0300

    Inventário sendo feito

--- Zeldinha Protótipo/Assets/Scripts/ChengeWeapon.cs
+++ Zeldinha Protótipo/Assets/Scripts/ChengeWeapon.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AtackScript : MonoBehaviour
+public class ChengeWeapon : MonoBehaviour
 {
-    [SerializeField] float damege;
-    [SerializeField] PlayerMov playerMov;
-    [SerializeField] EnemyController enemyController;
-
     // Start is called before the first frame update
     void Start()
     {
